2e6185b7df6712674cd67d71dc2728b9b8ddaa46
Arely Miramontes Rodriguez <arelymts@gmail.com>
2019-09-16 23:48:47 -0500

    register new users with token JWT AUTH

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
 // User Model
 const User = require('../../models/User');
@@ -34,13 +36,23 @@ router.post('/', (req, res) => {
                     newUser.password = hash;
                     newUser.save()
                         .then(user => {
-                            res.json( {
-                                user: {
-                                    id: user.id,
-                                    name: user.name,
-                                    email: user.email
+
+                            jwt.sign(
+                                { id: user.id },
+                                config.get('jwtSecret'),
+                                { expiresIn: 3600 },
+                                (err, token) => {
+                                    if(err) throw err;
+                                    res.json( {
+                                        token,
+                                        user: {
+                                            id: user.id,
+                                            name: user.name,
+                                            email: user.email
+                                        }
+                                    });
                                 }
-                            });
+                            )
                         })
                 })
             });
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const config = require('config');
+
 const app = express();
 
 // Bodyparser Middleware
 app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 // Connect to MongoDB
 mongoose
