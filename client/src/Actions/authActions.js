f2454ab05eea41402275581c2a71981ae5f8290c
Arely Miramontes Rodriguez <arelymts@gmail.com>
2019-09-17 22:01:57 -0500

    create config token help function

--- client/src/Actions/authActions.js
+++ client/src/Actions/authActions.js
@@ -16,6 +16,21 @@ export const loadUser = () => (dispatch, getState) => {
     // User loading
     dispatch({ type: USER_LOADING });
 
+    axios.get('/api/auth/user', tokenConfig(getState))
+        .then(res => dispatch({
+            type: USER_LOADED,
+            payload: res.data
+        }))
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({
+                type: AUTH_ERROR
+            })
+        })
+};
+
+// Setup config/headers and token
+export const tokenConfig = getState => {
     // Get token from localstorage
     const token = getState().auth.token;
 
@@ -31,15 +46,5 @@ export const loadUser = () => (dispatch, getState) => {
         config.headers['x-auth-token'] = token;
     }
 
-    axios.get('/api/auth/user', config)
-        .then(res => dispatch({
-            type: USER_LOADED,
-            payload: res.data
-        }))
-        .catch(err => {
-            dispatch(returnErrors(err.response.data, err.response.status));
-            dispatch({
-                type: AUTH_ERROR
-            })
-        })
+    return config;
 }
