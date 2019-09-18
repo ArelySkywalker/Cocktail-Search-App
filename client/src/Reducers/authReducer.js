5b2949494ac6f831ec92bf2dc5bb7c87271c61be
Arely Miramontes Rodriguez <arelymts@gmail.com>
2019-09-17 22:05:05 -0500

    on login and register success store token

--- client/src/Reducers/authReducer.js
+++ client/src/Reducers/authReducer.js
@@ -32,6 +32,7 @@ export default function (state = initialState, action) {
             };
         case LOGIN_SUCCESS:
         case REGISTER_SUCCESS:
+            localStorage.setItem('token', action.payload.token);
             return {
                 ...state,
                 ...action.payload,
